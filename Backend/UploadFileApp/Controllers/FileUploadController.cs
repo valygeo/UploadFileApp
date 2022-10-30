5b2722be54d6abf7fc6deaec8ad54aa57708fc29
Vali <94057367+valygeo@users.noreply.github.com>
2022-10-30 15:46:01 +0200

    -controller for upload file request created

---
+++ Backend/UploadFileApp/Controllers/FileUploadController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UploadFileApp.Models;
+
+namespace UploadFileApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+       
+        public FileUploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        [HttpPost("UploadFile")]
+
+        public async Task<string> Post([FromForm]FileUpload objFile)
+        {
+            try
+            {
+                if (objFile.files.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\Upload\\" + objFile.files.FileName))
+                    {
+                        objFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "\\Upload\\" + objFile.files.FileName;
+                    }
+                }
+                else
+                {
+                    return "Failed!";
+                }
+            }
+            catch (Exception exception)
+            {
+
+                return exception.Message.ToString();
+            }
+        }
+
+       
+    }
+    
+}
