f78e4c450c07a52ae8861798a6b019da62577ed2
Vali <94057367+valygeo@users.noreply.github.com>
2022-11-05 14:20:46 +0200

    -added endpoint for read file

--- Backend/UploadFileApp/Controllers/FileUploadController.cs
+++ Backend/UploadFileApp/Controllers/FileUploadController.cs
@@ -17,6 +17,34 @@ namespace UploadFileApp.Controllers
             _environment = environment;
             _basePath = _environment.WebRootPath + "\\Upload\\";
         }
+
+
+        [HttpGet("GetFile")]
+        public ActionResult <FileUpload> GetFile([FromForm]FileUpload file)
+        {
+            //if (Directory.Exists(_basePath))
+            //{
+            //    return (Ok(Directory.EnumerateFiles(_basePath)));
+            //}
+            //return BadRequest("directory not found!");
+            if(Directory.Exists(_basePath))
+            {
+                if(file.files.Length > 0)
+                {
+                    if (System.IO.File.Exists(_basePath + file.files.FileName) == true)
+                    {
+                       var stream = System.IO.File.OpenRead(_basePath+file.files.FileName);
+                        return Ok(stream);
+                    }
+                    return BadRequest("file doesn't exist!");
+                }
+                return BadRequest("file is empty!");
+            }
+            return BadRequest("directory doesn't exist!");
+
+
+        }
+
         [HttpPost("UploadFile")]
 
         public async Task<string> Post([FromForm]FileUpload objFile)
@@ -29,6 +57,7 @@ namespace UploadFileApp.Controllers
                     {
                         Directory.CreateDirectory(_basePath);
                     }
+                    
                     if (System.IO.File.Exists(_basePath + objFile.files.FileName) == false)
                     {
                         using (FileStream fileStream = System.IO.File.Create(_basePath + objFile.files.FileName))
