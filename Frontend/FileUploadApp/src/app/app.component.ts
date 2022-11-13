3dbc24127c08be1693bc069d9cdcd6f8bf69cfbb
Vali <94057367+valygeo@users.noreply.github.com>
2022-11-13 11:46:59 +0200

    -started implementation of upload file app

--- Frontend/FileUploadApp/src/app/app.component.ts
+++ Frontend/FileUploadApp/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FileUploadApp';
+ name:string='';
+ file:any;
+ getName(name:string){
+  this.name=name;
+ }
+ getFile(event:any){
+  this.file=event.target.files[0];
+  console.log(this.file);
+ }
 }
+
+
+
